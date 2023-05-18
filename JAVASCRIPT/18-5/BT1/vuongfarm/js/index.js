75bcfd9990df61d3c8186c21c62f637d224d4619
dothanhtu1804 <dothanhtu.1804@gmail.com>
2023-05-18 15:07:13 +0700

    [18-5] complete 18-5

--- JAVASCRIPT/18-5/BT1/vuongfarm/js/index.js
+++ JAVASCRIPT/18-5/BT1/vuongfarm/js/index.js
@@ -1,10 +1,6 @@
-
-
 document
   .getElementsByClassName("right_product_list")
-  .addEventListener("load", fetchData());
-
-
+  .addEventListener("load", fetchDataSingle(1));
 
 fetch("https://fakestoreapi.com/products/${id}")
   .then((res) => res.json())
@@ -14,11 +10,16 @@ async function fetchDataSingle(id) {
   const product = await fetch(`https://fakestoreapi.com/products/${id}`).then(
     (res) => res.json()
   );
-  
+
+  const queryString = window.location.search;
+  console.log(queryString);
+  const urlParams = new URLSearchParams(queryString);
+  console.log(urlParams);
+  // const id = urlParams.get("id");
   const productsDiv = document.querySelector(".product_list");
   console.log(product);
   // for (let item of products) {
-    productsDiv.innerHTML += `
+  productsDiv.innerHTML += `
     <div class="product_card" onclick="goToDetail(${product.id})">
         <img src="${product.image}" alt="" />
         <div class="detail_product_card">
---
+++ JAVASCRIPT/18-5/BT1/vuongfarm/js/login.js
@@ -0,0 +1,35 @@
+window.localStorage.removeItem('token');
+
+
+function login() {
+  username = document.getElementById("username").value;
+  password = document.getElementById("password").value;
+  fetch("https://dummyjson.com/auth/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username: username,
+      password: password,
+    }),
+  })
+  .then(
+    function(response) {
+        console.log(response);
+      if (response.status !== 200) {
+        console.log('Lỗi, mã lỗi ' + response.status);
+        return;
+      }
+      // parse response data
+      response.json().then(data => {
+        console.log(data.token);
+        if(data.token != null || data.token != undefined || data.token != ""){
+            localStorage.setItem('token', data.token)
+            window.location.replace("./index.html");
+        }
+      })
+    }
+  )
+  .catch(err => {
+    console.log('Error :-S', err)
+  });
+}
